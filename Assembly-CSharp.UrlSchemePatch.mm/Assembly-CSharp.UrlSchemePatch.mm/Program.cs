5d4934d1bf2ef218a5bb2e9af79ee6855c2d1183
aub <auburnete@gmail.com>
2020-04-25 08:51:16 +1000

    Add HTTP Server Hello World when you're in the import screen
    The plan is to patch RD to expose a http server which will do RPC.
    Then, all the helper needs to do is:
    
    if RD isn't running:
     - start RD with the rhythmdr:// link as the argument
    if RD is running:
     - send an RPC POST to RD to tell it to add a new URL to the import
    
    Right now it just does a HelloWorld -- and a synchronous Hello World
    no less, but at the very least I've got a http server running.

---
+++ Assembly-CSharp.UrlSchemePatch.mm/Assembly-CSharp.UrlSchemePatch.mm/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using MonoMod;
+
+using Assembly_CSharp;
+using UnityEngine;
+
+
+#pragma warning disable CS0626
+namespace Assembly_CSharp
+{
+
+    [MonoModPatch("global::scnLogo")]
+    class patch_scnLogo : scnLogo
+    {
+        private extern void orig_Exit();
+        private void Exit()
+        {
+            Debug.Log("Hi from MonoMod! (URL Scheme Patch 0.0.1)");
+            orig_Exit();
+        }
+    }
+
+    [MonoModPatch("global::LevelImporter")]
+    class patch_LevelImporter : LevelImporter
+    {
+
+        public HttpListener myListener;
+
+        public extern void orig_set_Showing(bool showing);
+
+        public void set_Showing(bool showing)
+        {
+            Debug.Log("The showing has been hit!");
+            Debug.Log(showing);
+
+            if (showing == true)
+            {
+                myListener = new HttpListener();
+                myListener.Prefixes.Add("http://localhost:49812/");
+                myListener.Start();
+                Debug.Log("Waiting for HTTP...");
+                HttpListenerContext context = myListener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            else
+            {
+                myListener.Stop();
+            }
+
+            orig_set_Showing(showing);
+        }
+    }
+}
